d99eb293ff50937f7f86adb8f52a74c53c6710fe
unknown <freddy.luo@mail.utoronto.ca>
2020-12-02 22:33:30 -0500

    heroku deploy attempt

--- server.js
+++ server.js
@@ -22,8 +22,22 @@ mongoose.set('useFindAndModify', false); // for some deprecation issues
 const { ObjectID } = require("mongodb");
 
 
+/*** Webpage routes below **********************************/
+// Serve the build
+app.use(express.static(path.join(__dirname, "/client/build")));
 
+// All routes other than above will go to index.html
+app.get("*", (req, res) => {
+    // check for page routes that we expect in the frontend to provide correct status code.
+    const goodPageRoutes = ["/", "/login", "/dashboard"];
+    if (!goodPageRoutes.includes(req.url)) {
+        // if url not in expected page routes, set status to 404.
+        res.status(404);
+    }
 
+    // send index.html
+    res.sendFile(path.join(__dirname, "/client/build/index.html"));
+});
 
 
 /*************************************************/
