056642539b9660e291b96159247f1fde497cf777
unknown <freddy.luo@mail.utoronto.ca>
2020-12-04 18:05:48 -0500

    session time extention, deployed routes added

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.use(
         resave: false,
         saveUninitialized: false,
         cookie: {
-            expires: 300000, //5 min
+            expires: 1800000, //30 min
             httpOnly: true
         }
     })
@@ -270,7 +270,7 @@ app.use(express.static(path.join(__dirname, "/client/build")));
 // All routes other than above will go to index.html
 app.get("*", (req, res) => {
     // check for page routes that we expect in the frontend to provide correct status code.
-    const goodPageRoutes = ["/", "/login", "/main"];
+    const goodPageRoutes = ["/", "/login", "/main", "/signup", "/blog", "/settings", "/bookmarks", "/profile"];
     if (!goodPageRoutes.includes(req.url)) {
         // if url not in expected page routes, set status to 404.
         res.status(404);
