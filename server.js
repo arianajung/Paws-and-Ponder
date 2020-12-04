735a7b07081ae3e951020b9be726e3ca20380f72
Ariana Jung <syjung0904@gmail.com>
2020-12-03 23:42:08 -0500

    Forgot to push server files

--- server.js
+++ server.js
@@ -4,17 +4,17 @@ const log = console.log;
 const express = require("express");
 // starting the express server
 const app = express();
-const path = require('path')
+const path = require("path");
 
 // mongoose and mongo connection
 const { mongoose } = require("./db/mongoose");
-mongoose.set('useFindAndModify', false); // for some deprecation issues
+mongoose.set("useFindAndModify", false); // for some deprecation issues
 
 // Refer to the below link to study middle ware and session setups.
 // https://github.com/csc309-fall-2020/react-express-authentication/blob/master/server.js
 
 const { User } = require("./models/user");
-const { Post } = require("./models/post");
+const { Comment, Post, Tag } = require("./models/post");
 const { Image } = require("./models/image");
 
 // to validate object IDs
@@ -28,41 +28,46 @@ app.use(bodyParser.json());
 const session = require("express-session");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-function isMongoError(error) { // checks for first error returned by promise rejection if Mongo database suddently disconnects
-    return typeof error === 'object' && error !== null && error.name === "MongoNetworkError"
+function isMongoError(error) {
+    // checks for first error returned by promise rejection if Mongo database suddently disconnects
+    return (
+        typeof error === "object" &&
+        error !== null &&
+        error.name === "MongoNetworkError"
+    );
 }
 
 // middleware for mongo connection error for routes that need it
 const mongoChecker = (req, res, next) => {
     // check mongoose connection established.
     if (mongoose.connection.readyState != 1) {
-        log('Issue with mongoose connection')
-        res.status(500).send('Internal server error')
+        log("Issue with mongoose connection");
+        res.status(500).send("Internal server error");
         return;
     } else {
-        next()
+        next();
     }
-}
-
+};
 
 // Middleware for authentication of resources
 const authenticate = (req, res, next) => {
     if (req.session.user_id) {
-        User.findById(req.session.user_id).then((user) => {
-            if (!user) {
-                return Promise.reject()
-            } else {
-                req.user = user
-                next()
-            }
-        }).catch((error) => {
-            res.status(401).send("Unauthorized")
-        })
+        User.findById(req.session.user_id)
+            .then((user) => {
+                if (!user) {
+                    return Promise.reject();
+                } else {
+                    req.user = user;
+                    next();
+                }
+            })
+            .catch((error) => {
+                res.status(401).send("Unauthorized");
+            });
     } else {
-        res.status(401).send("Unauthorized")
+        res.status(401).send("Unauthorized");
     }
-}
-
+};
 
 /*** Session handling **************************************/
 // Create a session and session cookie
@@ -73,8 +78,8 @@ app.use(
         saveUninitialized: false,
         cookie: {
             expires: 600000, //10 min
-            httpOnly: true
-        }
+            httpOnly: true,
+        },
     })
 );
 
@@ -87,26 +92,29 @@ app.post("/users/login", (req, res) => {
     // Use the static method on the User model to find a user
     // by their username and password
     User.findByUsernamePassword(username, password)
-        .then(user => {
+        .then((user) => {
             // Add the user's id to the session.
             // We can check later if this exists to ensure we are logged in.
             req.session.user_id = user._id;
             req.session.username = user.username;
-            res.send({ currentUser: user.username });
+            res.send({
+                currentUser: user.username,
+                curr_uid: user._id, // also send id
+            });
         })
-        .catch(error => {
-            res.status(400).send()
+        .catch((error) => {
+            res.status(400).send();
         });
 });
 
 // A route to logout a user
 app.get("/users/logout", (req, res) => {
     // Remove the session
-    req.session.destroy(error => {
+    req.session.destroy((error) => {
         if (error) {
             res.status(500).send(error);
         } else {
-            res.send()
+            res.send();
         }
     });
 });
@@ -120,13 +128,11 @@ app.get("/users/check-session", (req, res) => {
     }
 });
 
-
-
 /*** API Routes below ************************************/
 // User API Route
 // a post route to *create* a User
-app.post('/api/addUser', mongoChecker, async (req, res) => {
-    log(`Adding User ${req.body.username}`)
+app.post("/api/addUser", mongoChecker, async (req, res) => {
+    log(`Adding User ${req.body.username}`);
 
     // Create a new user using the User mongoose model
     const user = new User({
@@ -138,25 +144,84 @@ app.post('/api/addUser', mongoChecker, async (req, res) => {
         follower: [],
         userPosts: [],
         bookmarks: [],
-        bio: req.body.bio
-    })
+        bio: req.body.bio,
+    });
 
     // Save user to the database
     // async-await version:
     try {
-        const result = await user.save()
-        res.send(result)
+        const result = await user.save();
+        res.send(result);
     } catch (error) {
-        log(error) // log server error to the console, not to the client.
-        if (isMongoError(error)) { // check for if mongo server suddenly dissconnected before this request.
-            res.status(500).send('Internal server error')
+        log(error); // log server error to the console, not to the client.
+        if (isMongoError(error)) {
+            // check for if mongo server suddenly dissconnected before this request.
+            res.status(500).send("Internal server error");
         } else {
-            res.status(400).send('Bad Request') // 400 for bad request gets sent to client.
+            res.status(400).send("Bad Request"); // 400 for bad request gets sent to client.
         }
     }
-})
+});
 
+// a route to get all posts for main (posts from all users the current user follows)
+app.get(
+    "/api/get-main-posts/:user_id",
+    mongoChecker,
+    authenticate,
+    async (req, res) => {
+        const user_id = req.params.user_id;
 
+        // Good practise: Validate id immediately.
+        if (!ObjectID.isValid(user_id)) {
+            res.status(404).send(); // if invalid id, definitely can't find resource, 404.
+            return; // so that we don't run the rest of the handler.
+        }
+
+        // array of id's of users that the current user follows
+        let followingUsersArray;
+
+        try {
+            const followingUsers = await User.findOne({ _id: user_id })
+                .populate("following")
+                .exec();
+            if (!followingUsers) {
+                res.status(404).send("Resource not found");
+            } else {
+                followingUsersArray = followingUsers.following.map((user) => {
+                    return user._id;
+                });
+            }
+        } catch (error) {
+            log(error); // log server error to the console, not to the client.
+            if (isMongoError(error)) {
+                // check for if mongo server suddenly dissconnected before this request.
+                res.status(500).send("Internal server error");
+            } else {
+                res.status(400).send("Bad Request"); // 400 for bad request gets sent to client.
+            }
+        }
+
+        // find all posts that have its owner value with the user_id's in followingUsersArray
+        try {
+            const posts = await Post.find({
+                owner_id: { $in: followingUsersArray },
+            }).populate();
+            if (!posts) {
+                res.status(404).send("Resource not found");
+            } else {
+                res.send({ posts });
+            }
+        } catch (error) {
+            log(error); // log server error to the console, not to the client.
+            if (isMongoError(error)) {
+                // check for if mongo server suddenly dissconnected before this request.
+                res.status(500).send("Internal server error");
+            } else {
+                res.status(400).send("Bad Request"); // 400 for bad request gets sent to client.
+            }
+        }
+    }
+);
 
 // ********************* API Routes End Here **********************************
 
@@ -176,7 +241,6 @@ app.get("*", (req, res) => {
     // send index.html
     res.sendFile(path.join(__dirname, "/client/build/index.html"));
 });
-
 
 /*************************************************/
 // Express server listening...
