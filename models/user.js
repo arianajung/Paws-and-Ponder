c869d5ca3ef354e0ec8f21131c3d178c79583064
Ariana Jung <syjung0904@gmail.com>
2020-12-03 21:15:08 -0500

    Modified mongoose schemas

--- models/user.js
+++ models/user.js
@@ -1,8 +1,8 @@
-const mongoose = require('mongoose')
-// to validate object IDs
+const mongoose = require("mongoose");
+// to validate object ID
 const { ObjectID } = require("mongodb");
 
-const bcrypt = require('bcryptjs')
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
     username: {
@@ -16,13 +16,13 @@ const UserSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true,
-        minlength: 4
-    }, 
+        minlength: 4,
+    },
     profileImg: String, // ?
-    following: [{ type: ObjectID, ref: 'User' }],
-    follower: [{ type: ObjectID, ref: 'User' }],
-    userPosts: [{ type: ObjectID, ref: 'Post' }],
-    bookmarks: [{ type: ObjectID, ref: 'Post' }],
+    following: [{ type: ObjectID, ref: "User" }],
+    follower: [{ type: ObjectID, ref: "User" }],
+    userPosts: [{ type: ObjectID, ref: "Post" }],
+    bookmarks: [{ type: ObjectID, ref: "Post" }],
     // Unsure about the ones below
     bio: String,
 });
@@ -30,49 +30,47 @@ const UserSchema = new mongoose.Schema({
 // An example of Mongoose middleware.
 // This function will run immediately prior to saving the document
 // in the database.
-UserSchema.pre('save', function(next) {
-	const user = this; // binds this to User document instance
+UserSchema.pre("save", function (next) {
+    const user = this; // binds this to User document instance
 
-	// checks to ensure we don't hash password more than once
-	if (user.isModified('password')) {
-		// generate salt and hash the password
-		bcrypt.genSalt(10, (err, salt) => {
-			bcrypt.hash(user.password, salt, (err, hash) => {
-				user.password = hash
-				next()
-			})
-		})
-	} else {
-		next()
-	}
-})
-
+    // checks to ensure we don't hash password more than once
+    if (user.isModified("password")) {
+        // generate salt and hash the password
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        next();
+    }
+});
 
 // A static method on the document model.
 // Allows us to find a User document by comparing the hashed password
 //  to a given one, for example when logging in.
-UserSchema.statics.findByUsernamePassword = function(username, password) {
-	const User = this // binds this to the User model
+UserSchema.statics.findByUsernamePassword = function (username, password) {
+    const User = this; // binds this to the User model
 
-	// First find the user by their username
-	return User.findOne({ username: username }).then((user) => {
-		if (!user) {
-			return Promise.reject()  // a rejected promise
-		}
-		// if the user exists, make sure their password is correct
-		return new Promise((resolve, reject) => {
-			bcrypt.compare(password, user.password, (err, result) => {
-				if (result) {
-					resolve(user)
-				} else {
-					reject()
-				}
-			})
-		})
-	})
-}
-
+    // First find the user by their username
+    return User.findOne({ username: username }).then((user) => {
+        if (!user) {
+            return Promise.reject(); // a rejected promise
+        }
+        // if the user exists, make sure their password is correct
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, result) => {
+                if (result) {
+                    resolve(user);
+                } else {
+                    reject();
+                }
+            });
+        });
+    });
+};
 
 // make a model using the User schema
-const User = mongoose.model('User', UserSchema)
-module.exports = { User }
+const User = mongoose.model("User", UserSchema);
+module.exports = { User };
