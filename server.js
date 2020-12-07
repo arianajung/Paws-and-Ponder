d0bcc4c60591a697d1dc406342ff9b31b4d27624
unknown <freddy.luo@mail.utoronto.ca>
2020-12-06 22:23:59 -0500

    verify admin for deleting comments

--- server.js
+++ server.js
@@ -494,12 +494,18 @@ app.delete(
             const foundPost = await Post.findOne({ _id: postID });
             if (!foundPost) {
                 res.status(404).send("Comment not found");
-            } else if (!foundPost.owner_id.equals(req.user._id) && req.user.role !== "admin") {
-                res.status(401).send("Unauthorized");
             } else {
-                const comment = foundPost.comments.id(commentID).remove();
-                const post = await foundPost.save();
-                res.send({ post, comment });
+                const comment = foundPost.comments.id(commentID)
+                console.log(comment)
+                if (!comment) {
+                    res.status(404).send("Comment not found");
+                } else if (!comment.owner_id.equals(req.user._id) && req.user.role !== "admin") {
+                    res.status(401).send("Unauthorized");
+                } else {
+                    await comment.remove();
+                    const post = await foundPost.save();
+                    res.send({ post, comment });
+                }
             }
         } catch (error) {
             log(error);
@@ -509,34 +515,7 @@ app.delete(
 );
 
 // ********************* Admin APIs **********************************
-app.delete(
-    "/api/removeCommentAdmin/:postID/:commentID",
-    mongoChecker, 
-    authenticate,
-    isAdmin,
-    async (req, res) => {
-        const postID = req.params.postID;
-        const commentID = req.params.commentID;
-        // Validate id
-        if (!ObjectID.isValid(postID) || !ObjectID.isValid(commentID)) {
-            res.status(401).send("Post or comment not found");
-            return;
-        }
-        try {
-            const foundPost = await Post.findOne({ _id: postID });
-            if (!foundPost) {
-                res.status(404).send("Comment not found");
-            } else {
-                const comment = foundPost.comments.id(commentID).remove();
-                const post = await foundPost.save();
-                res.send({ post, comment });
-            }
-        } catch (error) {
-            log(error);
-            res.status(500).send(); // server error, could not delete.
-        }
-    }
-);
+// if any
 
 
 
